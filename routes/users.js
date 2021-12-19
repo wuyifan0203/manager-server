8c576ae22abecbc4ce457b8886ebedb0bc9c7138
wuyifan <1208097313@qq.com>
2021-12-20 00:09:04 +0800

    feat:完成user表，登录接口

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const router = require('koa-router')()
+const user = require('../models/userSchema')
+const { success, fail } = require('../utils/erroCode')
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+router.post('/login', async (ctx, next) => {
+  try {
+    const { userName, password } = ctx.request.body
+    const res = await user.findOne({ userName, password })
+    if (res) {
+      ctx.body = success(res)
+    } else {
+      ctx.body = fail('username or password is wrong!')
+    }
+  } catch (error) {
+    ctx.body = fail('query fail because ' + error.msg)
+  }
 })
 
 router.get('/bar', function (ctx, next) {
